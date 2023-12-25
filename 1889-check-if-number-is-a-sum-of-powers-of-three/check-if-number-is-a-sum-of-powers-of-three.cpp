cbe724ccc3a9a3852653f506751df29fa4fe4e75
vmk8826 <68069180+vmk8826@users.noreply.github.com>
2023-12-25 23:35:17 +0530

    Time: 3 ms (25.00%) | Memory: 6.2 MB (98.77%) - LeetSync

--- 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.cpp
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -1,24 +1,10 @@
 class Solution {
 public:
-    std::unordered_map<int,int>mp;
-    int getAns(int n,vector<int>vis){
-        if(n==0)return true;
-        if(n<0)return false;
-        if(mp.find(n)!=mp.end())return mp[n];
-        int ans=0;
-        for(int i=0;i<=15;i++){
-            if(!vis[i]){
-                vis[i]=1;
-                int curr=n-pow(3,i);
-                if(getAns(curr,vis)){
-                    return mp[n]=true;
-                }
-            }
-        }
-        return mp[n]=false;
-    }
     bool checkPowersOfThree(int n) {
-        vector<int>vis(16,0);
-        return getAns(n,vis);
+        while(n){
+            if(n%3>1)return false;
+            n=n/3;
+        }
+        return true;
     }
 };
