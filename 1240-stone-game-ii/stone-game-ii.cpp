7e88cf94ceb9ef295abe319cab982286617f1296
vmk8826 <68069180+vmk8826@users.noreply.github.com>
2023-12-26 09:42:08 +0530

    Time: 10 ms (80.21%) | Memory: 8.6 MB (81.11%) - LeetSync

--- 1240-stone-game-ii/stone-game-ii.cpp
+++ 1240-stone-game-ii/stone-game-ii.cpp
@@ -7,17 +7,19 @@ public:
         if(dp[i][m]!=-1)return dp[i][m];
 
         int sum=0,maxi=INT_MIN,n=p.size();
-        
-        for(int x=0;x<2*m;x++){
-            if(i+x<p.size())sum+=p[i+x];
+
+        for(int x=0;x<min(2*m,n-i);x++){
+           sum+=p[i+x];
             maxi=max(maxi,sum-getMaxStones(i+x+1,max(m,x+1),p));
         }
 
         return dp[i][m]=maxi;
     }
     int stoneGameII(vector<int>& piles) {
+
         int sum = accumulate(piles.begin(),piles.end(),0);
         memset(dp,-1,sizeof(dp));
         return (sum+getMaxStones(0,1,piles))/2;
+
     }
 };
