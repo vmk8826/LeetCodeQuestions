0fbad72cff0cbd9a3f1cfbc3df3aec2831277275
vmk8826 <68069180+vmk8826@users.noreply.github.com>
2024-01-02 22:31:20 +0530

    Time: 83 ms (9.08%) | Memory: 9.3 MB (43.89%) - LeetSync

--- 1632-number-of-good-ways-to-split-a-string/number-of-good-ways-to-split-a-string.cpp
+++ 1632-number-of-good-ways-to-split-a-string/number-of-good-ways-to-split-a-string.cpp
@@ -1,13 +1,5 @@
 class Solution {
 public:
-    int isGood(unordered_map<int,int>mp1,unordered_map<int,int>&mp2){
-        for(auto it:mp2){
-            mp1[it.first]=mp1[it.first]-it.second;
-            if(mp1[it.first]==0)mp1.erase(it.first);
-        }
-        if(mp1.size()==mp2.size())return true;
-        return false;
-    }
     int getSplits(string s){
         unordered_map<int,int>dp,dp1;
         for(int i=0;i<s.size();i++){
@@ -16,7 +8,9 @@ public:
         int ans=0;
         for(int i=0;i<s.size();i++){
             dp1[s[i]-'a']++;
-            if(isGood(dp,dp1))ans++;
+            dp[s[i]-'a']--;
+            if(dp[s[i]-'a']==0)dp.erase(s[i]-'a');
+            if(dp1.size()==dp.size())ans++;
         }
         return ans;
     }
