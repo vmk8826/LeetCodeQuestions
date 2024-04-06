514b89b4ec6f9a47afee412989d7c9b4ff147ef5
vmk8826 <68069180+vmk8826@users.noreply.github.com>
2024-04-06 07:30:44 +0530

    Time: 150 ms (56.22%) | Memory: 9.7 MB (98.09%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int dp[301][301];
-    int getBurst(int i,int j,vector<int>&arr){
+    int getMaxCoins(int i,int j,vector<int>&arr){
         if(i>j)return 0;
         if(dp[i][j]!=-1)return dp[i][j];
-        long long ans=INT_MIN;
+        int ans=INT_MIN;
         for(int k=i;k<=j;k++){
-            // cout<<arr[k-1]<<" "<<arr[k]<<" "<<arr[k+1]<<endl;
-           long long curr=(arr[i-1]*arr[k]*arr[j+1])+getBurst(i,k-1,arr)+
-                    getBurst(k+1,j,arr);
-            ans=max(ans,curr);
+            ans=max(ans,arr[k]*arr[i-1]*arr[j+1]+
+            getMaxCoins(i,k-1,arr)+getMaxCoins(k+1,j,arr));
         }
         return dp[i][j]=ans;
     }
@@ -18,6 +16,6 @@ public:
         int n=nums.size();
         nums.insert(nums.begin(),1);
         nums.push_back(1);
-        return getBurst(1,n,nums);
+        return getMaxCoins(1,n,nums);
     }
 };
